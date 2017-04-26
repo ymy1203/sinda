22c0d6fdd94362fc54f57ac0d0ec3c16357dcfca
mingyang yao <mingyang yao>
2017-04-26 10:37:59 +0800

    合并前最后一次修改

--- js/src/wares.js
+++ js/src/wares.js
@@ -2,7 +2,8 @@ require(['vue', 'jquery', 'header', 'headerlogo', 'footer'], function(Vue){
 	new Vue({
 		el:"#app",
 		created(){
-			this.getData();
+			this.getData(this.goodsID);
+			// console.log(this.goodsID);
 		},
 		data(){
 			return{
@@ -11,6 +12,8 @@ require(['vue', 'jquery', 'header', 'headerlogo', 'footer'], function(Vue){
 				fuwu_provider:'',
 				//商品ID
 				goodsID:sessionStorage.goodsID,
+				//服务内容|商品评价的显示吟唱
+				or_show:true,
 			}
 		},
 		mounted(){
@@ -22,9 +25,9 @@ require(['vue', 'jquery', 'header', 'headerlogo', 'footer'], function(Vue){
 				var that = this;
 				$.ajax({
 					type: "post",
-                    url: "http://115.182.107.203:8088/xinda/xinda-api/product/package/detail",
+                    url: "/xinda-api/product/package/detail",
                     data: {
-                    	sId:'0cb85ec6b63b41fc8aa07133b6144ea3'
+                    	sId:id,
                     },
                     dataType: "json",
                     success: function(data) {
@@ -32,9 +35,34 @@ require(['vue', 'jquery', 'header', 'headerlogo', 'footer'], function(Vue){
                     	that.fuwu  = data.data;
                     	that.fuwu_product = data.data.product;
                     	that.fuwu_provider = data.data.providerProduct;
-                        // console.log(data.data);
+                    	// that.fuwu_provider = data.data.providerProduct;
+                        // console.log(id);
                     }
 				})
+				//这是评论的
+				$.ajax({
+					type: "post",
+                    url: "/xinda-api/product/judge/grid",
+                    data: {
+                    	start:0,
+						limit:10,
+						serviceId:id,
+						type:1,
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                    	//处理返回数据----0级；
+                    	console.log(data);
+                    }
+				})
+			},
+			//立即购买加入购物车页签切换
+			surInfo(bool){
+				if(bool){
+					this.or_show = true;
+				}else{
+					this.or_show = false;
+				}
 			}
 		}
 	})
