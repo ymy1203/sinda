ac762fc3e937261163d6ee5215faeb1db35b42bc
ganzhongyu <yingzigzy@163.com>
2017-04-27 11:46:48 +0800

    ..

--- js/src/shoppingcart.js
+++ js/src/shoppingcart.js
@@ -8,23 +8,69 @@ require(['vue', 'jquery', 'header', 'headerlogo', 'footer'], function(Vue) {
         data(){
         	return{
                 num:1,
-                total:""
+                total:"",
+                list:"",
         	}
         },
         mounted(){
 			$(".allCP").css("color","#2693d4");
+            this.getlist()
 		},
 		methods:{
+            getlist(){
+                var that = this;
+                $.ajax({
+                    type: "post",
+                    url: "/xinda-api/cart/list",
+                    data: {},
+                    dataType: "json",
+                    success: function(data) {
+                        //处理返回数据----0级；
+                        console.log(data)
+                        // that.fuwu  = data.data;
+                        that.list = data.data
+                        ID=
+                        console.log(that.list[0].unitPrice)
+                    }
+                })
+            },
             //数量加一
-            addNum:function(){
+            addNum(item){
                 this.num++;
+                console.log(item.id);
+                $.ajax({
+                    type: "post",
+                    url: "/xinda-api/cart/set",
+                    data: {
+                        id:item.id,
+                        num:this.num
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        //处理返回数据----0级；
+                        console.log(data)
+                    }
+                })
             },
             //数量减一
-            subNum:function(){
+            subNum(item){
                 this.num--;
                 if(this.num<=0){
                     this.num=0;
                 }
+                $.ajax({
+                    type: "post",
+                    url: "/xinda-api/cart/set",
+                    data: {
+                        id:item.id,
+                        num:this.num
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        //处理返回数据----0级；
+                        console.log(data)
+                    }
+                })
             },
             inputBlur(){
                 this.num = $("#inNum").val();
